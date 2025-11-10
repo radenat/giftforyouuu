1fce07d74a8ddd90b3b2c5f97cb5f694bb15ffb1
radenat <calistanafisaa@gmail.com>
2025-11-10 20:59:01 +0700

    Update main.js

--- asset/main.js
+++ asset/main.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'Hai, Athan'.split('')
+const text = 'Hai, Kaisen'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
