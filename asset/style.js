40a81d11db82331c8191c44d05fd579b07cd5aac
radenat <calistanafisaa@gmail.com>
2025-11-10 19:48:03 +0700

    Update style.js

--- asset/style.js
+++ asset/style.js
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "Thank you for loving me with all your lovee – ilysb bb!<33";
+            subtitle.innerText = "thank you for being with me, in your path of love— ilysm <3";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
